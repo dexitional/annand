5d541025036837dc9ea1e150db78bfe2ccd27967
dexitional <ebenezerkb.ackah@gmail.com>
2025-05-13 15:41:33 +0000

    fixed title and phone number

--- components/FooterSideRight.tsx
+++ components/FooterSideRight.tsx
@@ -8,7 +8,7 @@ function FooterSideRight({}: Props) {
     <div className="sm:px-10 sm:min-h-48 sm:min-w-[25rem]">
         <div className="flex flex-col md:space-y-10 ">
             <LogoFoot />
-            <p className="text-blue-900 font-bold">AnnanD Enterprise is passionate about harnessing the power of technology to drive business success and educational excellence. Working shape the future of IT consulting in Ghana and Beyond!</p>
+            <p className="text-blue-900 font-bold">annanD Consult is passionate about harnessing the power of technology to drive business success and educational excellence. Working shape the future of IT consulting in Ghana and Beyond!</p>
         </div>
         <div></div>
     </div>
